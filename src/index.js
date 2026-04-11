42a0a6cb37f3ebc252c4eb386cc80a99f7162008
harshipped <harsh.kumar0802@gmail.com>
2026-04-11 21:00:17 +0530

    utils code added

--- src/index.js
+++ src/index.js
@@ -8,27 +8,17 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Serer is running at port: ${process.env.PORT}`);
+    });
 
-/*
-import express from "express";
-
-const app = express();
-
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
     app.on("error", (error) => {
-      console.log("Error: ", error);
+      console.log("Server connection error ", error);
       throw error;
     });
-
-    app.listen(process.env.PORT, () => {
-      console.log(`App is listening on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error("ERROR: ", error);
-    throw error;
-  }
-})();
-*/
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed !! ", err);
+  });
