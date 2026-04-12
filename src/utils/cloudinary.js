aa1b9890ca16879da157e9b03f3585cb7a519d30
harshipped <harsh.kumar0802@gmail.com>
2026-04-12 21:15:46 +0530

    add controller logic

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,43 @@
+import { v2 as cloudinary } from "cloudinary";
+import { log } from "console";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    //file uploaded successfully
+    console.log("File is uploaded on Cloudinary");
+    console.log(response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // removes the locally saved temporary file as the upload failed
+    return null;
+  }
+};
+
+// // Upload an image
+// const uploadResult = await cloudinary.uploader
+//   .upload(
+//     "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
+//     {
+//       public_id: "shoes",
+//     }
+//   )
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// console.log(uploadResult);
+
+export { uploadOnCloudinary };
